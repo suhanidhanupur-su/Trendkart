3f1af4267d31f97269ca0a4fd1f14ee53e48ac46
Suhani kumari <suhanidhanupur@gmail.com>
2026-06-13 19:41:09 +0530

    Add

--- E_commerce/Trendkart/urls.py
+++ E_commerce/Trendkart/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from .views import *
 
 urlpatterns = [
 
+   path('', home, name='home'),
+   path('register/', register, name='register'),
 ]
